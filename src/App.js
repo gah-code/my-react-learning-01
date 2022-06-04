6e3d7afa15ba30ecd8193d3f971178d78148081b
Gilbert Haro <gilbertharocode@gmail.com>
2022-06-03 23:51:34 -0700

    practice-food-order-app Starter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
-
-function App() {
-  const ctx = useContext(AuthContext);
-
+function App () {
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
