63894b8e98ca5b782ac591c9d20bdc238c4519cd
Gilbert Haro <gilbertharocode@gmail.com>
2022-06-17 16:21:13 -0700

    End of Section - Building Custom React Hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './components/hooks/use-http';
 
-function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
+function App () {
+  const [tasks, setTasks] = useState ([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-4d71c-default-rtdb.firebaseio.com/tasks.json'
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp ();
+
+  useEffect (
+    () => {
+      const transformTasks = tasksObj => {
+        const loadedTasks = [];
+
+        for (const taskKey in tasksObj) {
+          loadedTasks.push ({id: taskKey, text: tasksObj[taskKey].text});
+        }
+
+        setTasks (loadedTasks);
+      };
+
+      fetchTasks (
+        {
+          url: 'https://react-http-4d71c-default-rtdb.firebaseio.com/tasks.json',
+        },
+        transformTasks
       );
+    },
+    [fetchTasks]
+  );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
+  const taskAddHandler = task => {
+    setTasks (prevTasks => prevTasks.concat (task));
   };
 
   return (
