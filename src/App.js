a7457eb1c71349fd3cdd6997d56b367f94e61eb2
Gilbert Haro <gilbertharocode@gmail.com>
2022-06-20 14:36:46 -0700

    Working with forms

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './components/hooks/use-http';
+import SimpleInput from './components/SimpleInput';
 
 function App () {
-  const [tasks, setTasks] = useState ([]);
-
-  const {isLoading, error, sendRequest: fetchTasks} = useHttp ();
-
-  useEffect (
-    () => {
-      const transformTasks = tasksObj => {
-        const loadedTasks = [];
-
-        for (const taskKey in tasksObj) {
-          loadedTasks.push ({id: taskKey, text: tasksObj[taskKey].text});
-        }
-
-        setTasks (loadedTasks);
-      };
-
-      fetchTasks (
-        {
-          url: 'https://react-http-4d71c-default-rtdb.firebaseio.com/tasks.json',
-        },
-        transformTasks
-      );
-    },
-    [fetchTasks]
-  );
-
-  const taskAddHandler = task => {
-    setTasks (prevTasks => prevTasks.concat (task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
 export default App;
-
-// Window.localStorage:
-// Allows one to access a Storage object for the Document's origin; the stored data is saved across browser sessions.
