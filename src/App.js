889cb043f5712c16ae73a83398b9873ff89351d9
Gilbert Haro <gilbertharocode@gmail.com>
2022-06-27 14:56:11 -0700

    Fetching meals via http, Section 17

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import {useState} from 'react';
 
-// import SimpleInput from './components/SimpleInput';
-import BasicInput from './components/BasicForm';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App () {
+  const [cartIsShown, setCartIsShown] = useState (false);
+
+  const showCartHandler = () => {
+    setCartIsShown (true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown (false);
+  };
+
   return (
-    <div className="app">
-      <BasicInput />
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
